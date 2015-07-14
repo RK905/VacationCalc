35a331b92401f8dfcc99105f74c9d035affdf593
RK905 <mr.rayenk@gmail.com>
2015-07-14 11:15:24 -0400

    Initial Commit

---
+++ VacationCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VacationCalc
+//
+//  Created by Rayen Kamta on 7/14/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VacationCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VacationCalc
+//
+//  Created by Rayen Kamta on 7/14/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
